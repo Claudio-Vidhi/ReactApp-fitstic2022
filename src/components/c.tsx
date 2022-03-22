32ad02ca6775c9327b0db85feb7fae8813f03c1f
Claudio-Vidhi <claudiovidhi7@gmail.com>
2022-03-22 12:47:48 +0100

    useState--> bottoni e contatore

--- src/components/c.tsx
+++ src/components/c.tsx
@@ -1,28 +1,83 @@
+import _ from "lodash";
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import "./cindex.scss";
 
-interface TestComponentProps{
-    readonly messaggio:string;
+interface TestComponentProps {
+  readonly messaggio: string;
 }
-export const TestComponent:React.FC<TestComponentProps> = (props) =>{
-    return <div className="testComponent">
-    testComponent
-    <div>Messaggio di Input: {props.messaggio} </div>
-    <TestComponentSenzaTag/>
-    </div>;
-    
+export const TestComponent: React.FC<TestComponentProps> = (props) => {
+  return (
+    <div className="testComponent">
+      testComponent
+      <div>Messaggio di Input: {props.messaggio} </div>
+      <TestComponentSenzaTag />
+    </div>
+  );
 };
-export const TestComponentSenzaTag=()=>{
-    //return <>TestComponent2</>
-    return <><div>div1</div>
-    <div>div2</div></>
+export const TestComponentSenzaTag = () => {
+  //return <>TestComponent2</>
+  return (
+    <>
+      <div>div1</div>
+      <div>div2</div>
+    </>
+  );
+};
+interface Post {
+  readonly autore: string;
+  readonly titolo: string;
+  readonly testo: string;
+};
+//definisco una lista di post
+export const PostList: Post[] = [
+  {
+    titolo: "Post",
+    autore: "Autore",
+    testo: "Testo",
+  },
+];
+
+ export interface PostComponentProps {
+  readonly post: Post;
+};
+export const PostComponent: React.FC<PostComponentProps> = (props) => {
+  return (
+    <div className="post">
+      <div className="title">{props.post.titolo}</div>
+      <div className="author">{props.post.autore}</div>
+      <div className="text">{props.post.testo}</div>
+    </div>
+  );
+};
+//cos'è che tipo è il props
+interface DateProps{
+    readonly date: Date;
+};
+//props variabile del input
+export const DateComponent: React.FC<DateProps> =(props)=>{
+    const currentDate = props.date.toString();//come viene visualizzato e come viene recuperato
+    return <div className="post">Data e Ora attuali: {currentDate}</div>;
+};
+
+interface CounterProps{
+  readonly initialValue: number;
+  readonly name: string;
+  readonly callback: (value: number)=>void;//qui è scritto come è fatta la callback ma non la funzione
 }
-interface PostList {
-    autore:string;
-    titolo:string;
-    commento:string;
-}
-export const Book =(libro:PostList) => {
-    return <div>{libro.autore} {libro.titolo} {libro.commento}</div>
-    
+export const CounterComponent: React.FC<CounterProps> = (props) =>{
+  const {initialValue, name, callback}=props;  
+  const clickHandler = (newValue: number) => {setValue(newValue);callback(newValue)};
+  //stato
+    const [value,setValue]= useState(initialValue);
+    return <div className="box">
+      <h3>{name}</h3>
+        Value: {value}
+        <button onClick={()=>{clickHandler(value+1)}}>Inc</button>
+        <button onClick={()=>{clickHandler(value-1)}}>Dec</button>
+        <button onClick={()=>{clickHandler(initialValue)}}>Reset</button>
+    </div>;
+};
+function setTempValue(newValue: number) {
+  throw new Error("Function not implemented.");
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,35 @@
 import "./index.scss";
 import ReactDOM from "react-dom";
-import _ from "lodash";
-import { Book, TestComponent, TestComponentSenzaTag } from "./components/c";
+import _, { initial } from "lodash";
+import { CounterComponent, DateComponent, PostComponent, PostList } from "./components/c";
+import { useState } from "react";
 
+//le props vengono ricevute devono essere passate dal padre al figlio
+//lo stato è intrinseco al componente e viene gestito da lui
 
-const App:React.FC = (props) => {
-  const numero= 4;
-  const messaggio1="messaggio1";
-  const autore1="Giovanni";
-  const titolo1="boh";
-  const commento1="WEH";
+const App:React.FC = () => {
+  const initialValue= 0;
+  const [date, setDate] = useState(new Date());
+  const [tempValue, setTempValue] = useState(initialValue);  
   return <div className='app'>
-  <div>app!</div>
-  <div>Numero: {numero}</div>
-  {numero === 4 && <div>condizione true</div>}
   <div className="wrapper">
-      <TestComponent messaggio={messaggio1} />
-      <TestComponent messaggio={messaggio1} />
-      <TestComponent messaggio={messaggio1}/>
-  </div>
-  <Book autore={autore1} titolo={titolo1} commento={commento1}/>
-    {/* <TestComponentSenzaTag /> */}
-</div>;
-};
+  {/* {PostList.map((post) => {
+      return <PostComponent post={post} />;
+    })} */}
+  <DateComponent date={date}/>
+  <button onClick={()=>{setDate(new Date());}}>Bottone</button>
 
+  {_.map(PostList, (post)=><PostComponent post={post}/>)}
+
+  <CounterComponent name={"Contatore"} initialValue={initialValue} callback={(value)=>{setTempValue(value)}}/>
+
+  <div className="wrapper">il valore del contatore è {tempValue}</div>
+  </div>
+    {/* <TestComponentSenzaTag /> */}
+  
+</div>
+  setInterval(()=>{},1000);
+};
 ReactDOM.render(<App/>, document.getElementById("root"));
+
+
